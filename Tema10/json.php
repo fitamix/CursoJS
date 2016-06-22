cc8d53708634f9d73e752679fc5a0d6c38fe7978
rafa <rafa.aparicio@gmail.com>
2016-06-23 00:21:39 +0200

    añadidos nuevos temas y ejemplos

---
+++ Tema10/json.php
@@ -0,0 +1,10 @@
+{
+	"respuesta": "OK",
+	"datos": {
+		"clave": "valor"
+	},
+	"array": [
+		{"a": 1},
+		{"b": 3}
+	]
+}
---
+++ Tema10/xml.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-type: text/xml");
+?>
+<respuesta>
+  <mensaje>Un mensaje</mensaje>
+  <parametros>
+    <telefono>555666111</telefono>
+    <codigo_postal>12345</codigo_postal>
+    <fecha_nacimiento>10-10-2000</fecha_nacimiento>
+  </parametros>
+</respuesta>
